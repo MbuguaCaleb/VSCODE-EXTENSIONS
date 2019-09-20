b02bddd7a094dc3ffed8f1ac6c84f7d74a6c6199
MbuguaCaleb <mbuguacaleb30@gmail.com>
2019-09-20 09:13:33 +0300

    initialCommit

---
+++ dist/js/main.js
@@ -0,0 +1,22 @@
+const name = 'Caleb';
+
+function name() {
+  console.log('test');
+}
+
+console.log('hello');
+console.error('error');
+
+const AddValue = (num1, num2) => {
+  console.log(num1 + num2);
+  if (num1) {
+  }
+};
+
+Add(1, 2);
+
+const arr = [1, 2, 3, 4, 5];
+
+arr.forEach(num => {
+  console.log(num);
+});
